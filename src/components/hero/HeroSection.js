43d7c120d67b4ca931747366e888b6dec7c4f6d3
Oloche Richard <98244432+Oloche-Richard@users.noreply.github.com>
2022-07-03 17:07:27 +0100

    initial commit

--- src/components/hero/HeroSection.js
+++ src/components/hero/HeroSection.js
@@ -1,10 +1,21 @@
-import hero from "./../image/ANIMAL BACKGROUND.jpg"
+import hero from "./../image/animalBackground.jpg"
 import  "./HeroSection.css"
 // import Button from "../Main/Button"
  const HeroSection = () => {
+    const heroStyle = {
+      backgroundImage: `url(${hero})`,
+      width: '100vw',
+      height: '70vh'
+
+    }
     return (
-      <div className='hero-section'>
-      <img src={hero} alt='hero image'/>
+      <div  className={heroStyle}>
+      <div>
+        <span>The Best</span>
+        <span>Custormer</span>
+        <span>Experiences</span>
+        <p>lorem slnsldfns lsanlnasisfnas inlsan lnalsnas ionasdfnoin asfnasinkl c ljnASOInaf sj sajnsonsdfson onSOfdnonsfdnlsknf sdfnoissdf dlnsnsd l;knoidsn </p>
+      </div>
       </div>
       
     )
